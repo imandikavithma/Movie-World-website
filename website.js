a972be1d2ffb579e06e5949f89cb7b8409bc2726
Imandi <imandikavithma2714@gmail.com>
2025-04-18 01:03:44 +0530

    Added JavaScript file for popAlert

---
+++ website.js
@@ -0,0 +1,14 @@
+function popAlert1() {  
+    var myName ="Welcome to Movie World English Movies";
+    alert(myName);
+  }
+  
+  function popAlert2() {  
+    var myName ="Welcome to Movie World Hindi Movies";
+    alert(myName);
+  }
+  
+  function popAlert3() {  
+    var myName ="Welcome to Movie World Telugu Movies";
+    alert(myName);
+  }
